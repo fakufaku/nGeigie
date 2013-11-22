58a976c6b835eb9e85eeffa285bb6cb5ab4ac223
robouden <rob@yr-design.biz>
2013-11-22 10:15:28 +0900

    Updated borad settings

--- src/netrad_ths/board_specific_settings.h
+++ src/netrad_ths/board_specific_settings.h
@@ -1,13 +1,33 @@
-#ifndef BOARD_SPECIFIC_SETTINGS_H
-#define BOARD_SPECIFIC_SETTINGS_H
 // NOTE:
-// Before uploading to your Arduino board,
+// Before uploading to your Arduino Ethernet board,
 // please replace with your own settings
 
 #define MAX_FAILED_CONNS 3
 
-// REPLACE WITH A PROPER MAC ADDRESS for the Etherenet PHY
-byte macAddress[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xBA, 0xBE };
+
+// Your API key (a public secure key is recommended)
+const char *apiKey = "z5H2f3pvUGf1zp81Gcmq";
+const char *lat = "35.6348" ;
+const char *lon = "139.7031" ;
+const char *ID = "38" ;
+
+// REPLACE WITH A PROPER MAC ADDRESS
+byte macAddress[] = { 0xDA, 0xD1, 0xBE, 0x60, 0x0D, 0x00 };
+
+
+// Enter an IP address for your controller below.
+// The IP address will be dependent on your local network:
+EthernetClient client;
+IPAddress localIP (192, 168, 100, 40);		// fallback local IP address if no DHCP
+IPAddress serverIP(176, 56, 236, 75 );
+
+
+// This is specific to the Uno Ethernet board
+int pinSpkr = 1;	// pin number of piezo speaker
+int pinLED = 0;		// pin number of event LED
+int resetPin = A1;
+//int radioSelect = A3;
+
 
 // Update interval in minutes
 const int updateIntervalInMinutes = 1;
@@ -15,10 +35,10 @@ const int updateIntervalInMinutes = 1;
 typedef struct
 {
   unsigned short addr;
-  char lat[20];
-  char lon[20];
-  char ID[20];
-  char firmware_version[10];
+  char lat[10];
+  char lon[10];
+  char ID[3];
+  char firmware_version[8];
 } device_t;
 
 typedef struct
@@ -27,30 +47,20 @@ typedef struct
     unsigned char conn_fail_cnt;
 } devctrl_t;
 
+
+
 enum states
 {
-	NORMAL	= 0,
-	RESET	= 1,
+    NORMAL = 0,
+    RESET = 1
 };
 
 // Tube model
-//LND712:	0.01
-//Inspector:	0.0029
-//SBM-20:	0.0067
-
-// The conversion coefficient from cpm to µSv/h
-const float conversionCoefficient = 0.0029;
+//const TubeModel tubeModel = LND_7313;
 
 // Interrupt mode:
 // * For most geiger counter modules: FALLING
 // * Geiger Counter Twig by Seeed Studio: RISING
 const int interruptMode = RISING;
 
-// NetRAD - this is specific to the NetRAD board
-const int pinSpkr = 1;	// pin number of piezo speaker
-const int pinLED = 0;		// pin number of event LED
-const int resetPin = A1;
-const int radioSelect = A3;
-
-#endif BOARD_SPECIFIC_SETTINGS_H
 // vim: set tabstop=4 shiftwidth=4 syntax=c foldmethod=marker :
